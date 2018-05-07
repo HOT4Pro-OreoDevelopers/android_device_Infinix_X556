2f3c16dd255bb7f151cfa55368090f6b72b71e97
Nasrellah Irhmar <nasreirma@gmail.com>
2018-05-07 18:15:07 +0100

    fix vendor mismatch

--- lineage.mk
+++ lineage.mk
@@ -24,3 +24,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-google
 
 # Available languages
 PRODUCT_LOCALES := ru_RU en_US en_GB tr_TR sk_SK vi_VN fr_FR ar_EG
+
+# Build fingerprint from Indian variant.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	BUILD_FINGERPRINT := Infinix/X556H371/X556:7.0/NRD90M/X556-H371A-N-171229V207:user/release-keys
